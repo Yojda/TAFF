9304de1f9916f3666de831eb777ecd53b5d0b614
moshi <leofornoff02@gmail.com>
2024-10-19 16:24:57 -0400

    App running - Leo

---
+++ testrail/src/main/java/ca/etsmtl/testrail/TestrailApplication.java
@@ -0,0 +1,13 @@
+package ca.etsmtl.testrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestrailApplication.class, args);
+	}
+
+}
---
+++ testrail/src/test/java/ca/etsmtl/testrail/TestrailApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.etsmtl.testrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
