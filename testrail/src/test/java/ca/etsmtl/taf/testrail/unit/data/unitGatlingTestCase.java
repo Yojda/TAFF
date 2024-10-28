e17943e423f0c0e6a0de42b916b799c10dab5811
yanth <yannis.ouakrim@telecomnancy.eu>
2024-10-28 00:46:33 -0400

    [test] unitGatlingTestCase

--- testrail/src/test/java/ca/etsmtl/taf/testrail/unit/data/unitGatlingTestCase.java
+++ testrail/src/test/java/ca/etsmtl/taf/testrail/unit/data/unitGatlingTestCase.java
@@ -1,5 +1,52 @@
 package ca.etsmtl.taf.testrail.unit.data;
+import ca.etsmtl.taf.testrail.model.entity.GatlingTestCase;
+import ca.etsmtl.taf.testrail.service.data.GatlingTestCaseService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class unitGatlingTestCase {
-    // TODO
+    private GatlingTestCaseService service;
+    @BeforeEach
+    void setUp() {
+        service = new GatlingTestCaseService();
+    }
+
+    @Test
+    void save() {
+        GatlingTestCase gatlingTestCase = new GatlingTestCase();
+        gatlingTestCase.setId(1L);
+        gatlingTestCase.setSectionId(1);
+        gatlingTestCase.setTitle("Test");
+
+        GatlingTestCase gatlingTestCaseSaved = service.save(gatlingTestCase);
+        assertEquals(gatlingTestCase, gatlingTestCaseSaved);
+    }
+
+    @Test
+    void findById() {
+        GatlingTestCase gatlingTestCase = new GatlingTestCase();
+        gatlingTestCase.setId(1L);
+        gatlingTestCase.setSectionId(1);
+        gatlingTestCase.setTitle("Test");
+
+        gatlingTestCase.setId(1L);
+        assertEquals(1L, gatlingTestCase.getId());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findBySectionId() {
+    }
+
+    @Test
+    void findByTitle() {
+    }
 }
