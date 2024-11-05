636d7c14a80930b831bfdab58502efd9d6ec07a9
moshi <leofornoff02@gmail.com>
2024-11-05 15:43:32 -0500

    Meeting

--- testrail/src/main/java/ca/etsmtl/taf/testrail/service/manager/saving/GeneralSavingManager.java
+++ testrail/src/main/java/ca/etsmtl/taf/testrail/service/manager/saving/GeneralSavingManager.java
@@ -5,6 +5,7 @@ import ca.etsmtl.taf.testrail.model.factory.TestRailProjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class GeneralSavingManager {
 
